27378d393837169e4863d043903b45a05478bd8b
LZAndy <87013958+LZAndy@users.noreply.github.com>
2024-04-20 12:43:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 from openxlab.model import download
 
-base_path = 'final_model/'
+base_path = './internlm-chat-1.8b-andy'
 os.system(f'git clone https://code.openxlab.org.cn/Andy_lee/internlm-chat-1.8b-andy.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
